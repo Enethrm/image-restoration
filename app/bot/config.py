914c787cd8244bcb29cae172a25c6b77199df1c4
respectedbastard <vn.semenov.e.s@gmail.com>
2024-06-05 09:22:07 +0300

    Added emoji in text

--- app/bot/config.py
+++ app/bot/config.py
@@ -1,5 +1,6 @@
-from configparser import ConfigParser
 import os
+from configparser import ConfigParser
+
 
 cfg = ConfigParser() 
 cfg.read(os.path.join(os.path.dirname(__file__), 'config.ini'),encoding='utf-8')
